48361e0408f95baa4ef7f710cae3054361c8538e
goldz9999 <adrianmiranda047@gmail.com>
2026-03-23 12:21:08 -0500

    Dia 5 Autenticacion y middleware

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function saludar(nombre) {
     return `Hola ${nombre}, bienvenido a Node`;
 }
@@ -108,4 +109,35 @@ const server = http.createServer((req, res) => {
 
 server.listen(5000, () => {
     console.log("Servidor ejecutándose en http://localhost:5000");
+=======
+
+const express = require('express');
+const menuRouter = require('./routes/menu.routes');
+const logger = require('./middlewares/logger');
+const conectarDB = require('./database/connection');
+const authRouter = require('./routes/auth.routes');
+const app = express();
+const PORT = 3000;
+
+// Conectar a MongoDB antes de todo
+conectarDB();
+
+app.use(express.json());
+app.use(logger);
+
+
+app.use('/menu', menuRouter);
+app.use('/auth', authRouter);
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        mensaje: 'Restaurante Node API',
+        version: '2.0.0',
+        rutas: ['/menu'] // puedes agregar '/auth' cuando lo tengas
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Restaurante corriendo en http://localhost:${PORT}`);
+>>>>>>> 9f5d744 (Dia 5 Autenticacion y middleware)
 });
