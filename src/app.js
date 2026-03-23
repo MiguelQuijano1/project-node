9396fc601851504325c578932cd1db28e0e8ca63
goldz9999 <adrianmiranda047@gmail.com>
2026-03-23 08:55:06 -0500

    Dia3

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function saludar(nombre) {
     return `Hola ${nombre}, bienvenido a Node`;
 }
@@ -109,3 +110,38 @@ const server = http.createServer((req, res) => {
 server.listen(5000, () => {
     console.log("Servidor ejecutándose en http://localhost:5000");
 });
+=======
+// app.js — solo configuración y conexión
+const express = require('express');
+const menuRouter = require('./routes/menu.routes');
+
+const app = express();
+const PORT = 3000;
+const logger = require('./middlewares/logger');
+
+app.use(express.json());
+app.use(logger);
+
+app.use('/menu', menuRouter);
+
+
+
+app.use(express.json());
+
+// Conectar el router del menú
+app.use('/menu', menuRouter);
+
+// Ruta de bienvenida
+app.get('/', (req, res) => {
+    res.status(200).json({
+        mensaje: 'Restaurante Node API',
+        version: '2.0.0',
+        rutas: ['/menu']
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Restaurante corriendo en http://localhost:${PORT}`);
+});
+
+>>>>>>> 6ca3d5b (Dia3)
